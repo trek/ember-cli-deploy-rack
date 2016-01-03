ec2eae0a7ae469ec0f9016029f29211c40c669cc
Maik Kempe <mkempe@bitaculous.com>
2016-01-03 15:10:36 +0100

    [➠] Version `1.0.1`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+1.0.1
+=====
+
+* [✓] Fixed version require
+
 1.0.0
 =====
 
--- lib/ember/cli/deploy/rack/version.rb
+++ lib/ember/cli/deploy/rack/version.rb
@@ -2,7 +2,7 @@ module Ember
   module CLI
     module Deploy
       module Rack # :nodoc:
-        VERSION = '1.0.0'
+        VERSION = '1.0.1'
       end
     end
   end
