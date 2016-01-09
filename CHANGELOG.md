a92a037bcccefd472dad240481aa8a867dba24bd
Maik Kempe <mkempe@bitaculous.com>
2016-01-09 22:53:00 +0100

    [➠] Version `2.2.0`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
-Unreleased
-==========
+2.2.0
+=====
 
 * [➠] Debug information can be enabled / disabled
 * [➠] Formatted debug information
--- lib/ember/cli/deploy/rack/version.rb
+++ lib/ember/cli/deploy/rack/version.rb
@@ -2,7 +2,7 @@ module Ember
   module CLI
     module Deploy
       module Rack # :nodoc:
-        VERSION = '2.1.0'
+        VERSION = '2.2.0'
       end
     end
   end
